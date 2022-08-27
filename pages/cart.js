199795fe71bde8054e63ce66e89ac33ca06aa7b2
Janusz Wozniak <januszwozniak.bth@gmail.com>
2022-08-27 16:56:34 +0100

    basket functionality

---
+++ pages/cart.js
@@ -0,0 +1,108 @@
+import Image from 'next/image';
+import Link from 'next/link'
+import React, { useContext } from 'react'
+import {XCircleIcon} from "@heroicons/react/outline"
+import Layout from '../components/Layout';
+import { Store } from '../utils/store';
+import dynamic from "next/dynamic"
+
+
+function CartScreen() {
+    const {state, dispatch} = useContext(Store);
+    const {
+        cart: {cartItems},
+    } = state
+    
+    const removeItemHandler = (item) => {
+        dispatch({type:"REMOVE_CART_ITEM", payload: item})
+    }
+    const updateCartHandler = (item, qty) => {
+        const quantity = parseInt(qty);
+        console.log({ quantity})
+        dispatch({type:"CART_ADD_ITEM", payload: {...item, quantity}})
+    }
+
+  return (
+    <Layout title="Shopping Cart">
+       <h1 className='mb-4 text-xl'>Shopping Cart</h1>
+       {
+        cartItems.length === 0 ? 
+        (<div>
+            Cart is empty. <Link href="/">Go Shopping</Link>
+        </div>) :
+        (<div className='grid md:grid-cols-4 md:gap-5'>
+            <div className='overflow-x-auto md:col-span-3'>
+                <table className='min-w-full'>
+                    <thead className='border-b'>
+                        <tr>
+                            <th className="px-5 text-left">Item</th>
+                            <th className="p-5 text-right">Quantity</th>
+                            <th className="p-5 text-right">Price</th>
+                            <th className="p-5">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cartItems.map((item) => (
+                            <tr key={item.slug} className="border-b">
+                                <td className='py-2'>
+                                    <Link href={`/product/${item.slug}`}>
+                                        <a className='flex items-center px-5'>
+                                            <Image
+                                                src={item.image}
+                                                alt={item.name}
+                                                width={50}
+                                                height={50}
+                                                
+                                            ></Image>
+                                            &nbsp;
+                                            - {item.name}
+                                        </a>
+                                    </Link>
+                                </td>
+                                <td className='text-right px-5'>
+                                    <select value={item.quantity} onChange={(e) => updateCartHandler(item, e.target.value)}>
+                                        {
+                                            [...Array(item.countInStock).keys()].map((x) => (
+                                                <option key={x+1} value={x+1}>
+                                                    {x+1}
+                                                </option>
+                                            ))
+                                        }
+                                    </select>
+                                </td>
+                                <td className='text-right px-5'>£{item.price * item.quantity}</td>
+                                <td className="p-5 flex justify-center"> 
+                                <button onClick={() => removeItemHandler(item)}>
+                                    <XCircleIcon className="h-6 w-6"></XCircleIcon>
+                                </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="card p-5">
+            <ul>
+              <li>
+                <div className="pb-3 text-xl">
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : $
+                  {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                </div>
+              </li>
+              <li>
+                <button
+                  onClick={() => router.push('/shipping')}
+                  className="primary-button w-full"
+                >
+                  Check Out
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>)
+       }
+    </Layout>
+  )
+}
+
+export default dynamic(() => Promise.resolve(CartScreen), {ssr: false});
---
+++ pages/product/[slug].js
@@ -0,0 +1,79 @@
+import React, { useContext } from "react";
+import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+import data from "../../utils/data";
+import Link from "next/link";
+import Image from "next/image";
+import { Store } from "../../utils/store";
+function ProductScreen() {
+    const {state, dispatch} = useContext(Store)
+    // console.log(state)
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find((x) => x.slug === slug); 
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug)
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert("Sorry. Product out of stock")
+      return
+    } 
+    dispatch({type:'CART_ADD_ITEM', payload:{...product, quantity} })
+  }
+  if (!product) {
+    return (
+        <Layout>
+                    <div>product not found</div>
+
+        </Layout>
+    )
+  }
+
+  
+
+  return (
+    <Layout title={product.title}>
+      <div className="py-3 ">
+        <Link href="/" >back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.description}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </div>
+        <div className="md:col-span-1">
+          <ul>
+            <li>{product.title}</li>
+            <li>Category: {product.category}</li>
+            <li>Brand: {product.brand}</li>
+            <li>
+              {product.rating} of {product.numReviews} reviews
+            </li>
+            <li>Description: {product.description}</li>
+          </ul>
+        </div>
+        <div>
+          <div className="card p-5">
+            <div className="mb-2 flex justify-between">
+              <div>Price</div>
+              <div>${product.price}</div>
+            </div>
+            <div className="mb-2 flex justify-between">
+              <div>Status</div>
+              <div>{product.countInStock > 0 ? "In stock" : "Unavailable"}</div>
+            </div>
+            <button className="primary-button w-full" onClick={addToCartHandler }>Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default ProductScreen;
+
